8fffa6e0ab79d411bd749d19d832df057e70ad16
Artek60744 <48151951+Artek60744@users.noreply.github.com>
2023-11-28 11:46:50 +0100

    p

--- systeme/player.cpp
+++ systeme/player.cpp
@@ -86,7 +86,7 @@ void Player::move(const float dir_x, const float dir_y){
 void Player::updatePhysique(){
     //Gravity
     this->velocity.y += 1.0 * this->gravity;
-    if (std::abs(this->velocity.y) > this->velocityMaxY)
+    if (std::abs(this->velocity.x) > this->velocityMaxY)
     {
         this->velocity.y = this->velocityMaxY * ((this->velocity.y < 0.f) ? -1.f : 1.f);
     }
